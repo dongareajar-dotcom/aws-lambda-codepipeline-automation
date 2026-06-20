5592fb068ca14635acea25facd84bfa0aa8dfdd3
Sahil Dongare <dongareajar@gmail.com>
2026-06-20 15:24:53 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -5,7 +5,7 @@ def lambda_handler(event, context):
     client = boto3.client('codepipeline')
 
     response = client.start_pipeline_execution(
-        name='autodeploy-lambda'
+        name='AutoDeployBuild'
     )
 
     return {
