c2a449d9fc0c4f5d28d0cebad1e3903a56211270
Sahil Dongare <dongareajar@gmail.com>
2026-06-20 15:19:47 +0530

    Create lambda_function.py

---
+++ lambda_function.py
@@ -0,0 +1,15 @@
+import boto3
+
+def lambda_handler(event, context):
+
+    client = boto3.client('codepipeline')
+
+    response = client.start_pipeline_execution(
+        name='
+autodeply-lambda'
+    )
+
+    return {
+        'statusCode': 200,
+        'body': 'Pipeline Triggered Successfully'
+    }
